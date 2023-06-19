924239d12ae2e6e88ac2d051ef6189715ebc8152
Faizan Patel <118436993+Faizan960@users.noreply.github.com>
2023-06-19 20:24:05 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,16 @@
+#continue 
+#for letter in 'Faizan Patel':
+	#if letter == 'e' or letter == 'a':
+#		continue
+#	print('Current Letter :', letter)
+
+#for letter in 'Faizan Patel':
+ 
+   
+  #  if letter == 'e' or letter == 'a':
+  #      break
+#print('Current Letter :', letter)
+
+for letter in 'Faizan Patel':
+    pass
+print('Last Letter :', letter)
